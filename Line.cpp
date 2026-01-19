f06a7c8e5baf5d34dac572717c1d89d0e5e02ec2
Alan Kwak <alankwak2@gmail.com>
2026-01-19 21:30:27 +0000

    implement toString() in Line

--- Line.cpp
+++ Line.cpp
@@ -1,4 +1,6 @@
 #include "Line.h"
+#include <sstream>
+#include <iomanip>
 #include <cmath>
 
 Line::Line(Point& p1, Point& p2)
@@ -64,5 +66,28 @@ void Line::extendLine(double value)
 
 string Line::toString() 
 {
-    return "";
+    double roundX1 = round(point1.getX() * 10.0) / 10.0;
+    double roundY1 = round(point1.getY() * 10.0) / 10.0;
+    stringstream streamx1;
+    stringstream streamy1;
+    streamx1 << fixed << setprecision(1) << roundX1;
+    streamy1 << fixed << setprecision(1) << roundY1;
+    string strX1 = streamx1.str();
+    string strY1 = streamy1.str();
+
+    double roundX2 = round(point2.getX() * 10.0) / 10.0;
+    double roundY2 = round(point2.getY() * 10.0) / 10.0;
+    stringstream streamx2;
+    stringstream streamy2;
+    streamx2 << fixed << setprecision(1) << roundX2;
+    streamy2 << fixed << setprecision(1) << roundY2;
+    string strX2 = streamx2.str();
+    string strY2 = streamy2.str();
+
+    double roundLen = round(getLength() * 10.0) / 10.0;
+    stringstream streamlen;
+    streamlen << fixed << setprecision(1) << roundLen;
+    string strLen = streamlen.str();
+
+    return "Line- Point 1: [X: " + strX1 + ", Y: " + strY1 + "], Point 2: [X: " + strX2 + ", Y: " + strY2 + "], Length: " + strLen;
 }
