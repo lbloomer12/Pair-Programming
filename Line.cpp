b6ec4008b1a1e175dbf19499da318bc0a382a3e5
Alan Kwak <alankwak2@gmail.com>
2026-01-20 19:24:56 +0000

    fix bug in containsPoint()

--- Line.cpp
+++ Line.cpp
@@ -2,6 +2,7 @@
 #include <sstream>
 #include <iomanip>
 #include <cmath>
+#include <iostream>
 
 Line::Line(Point& p1, Point& p2)
 {
@@ -27,13 +28,12 @@ double Line::getSlope()
     return (point1.getY() - point2.getY()) / (point1.getX() - point2.getX());
 }
 
-bool Line::containsPoint(Point p)
+bool Line::containsPoint(const Point& p)
 {
-    int x = p.getX(), y = p.getY();
-    bool inBoundsX = (x <= point1.getX() && x >= point2.getX()) || (x <= point2.getX() && x >= point1.getX());
-    bool inBoundsY = (y <= point1.getY() && y >= point2.getY()) || (y <= point2.getY() && y >= point1.getY());
-
-    if(inBoundsX && inBoundsY && ( (y-point1.getY()) / (x-point1.getX()) == getSlope() ) ) 
+    double x = p.getX(), y = p.getY();
+    bool inBoundsX = (x <= point2.getX() && x >= point1.getX());
+    
+    if(inBoundsX && ((y-point1.getY()) / (x-point1.getX())) == getSlope()) 
     {
         return true;
     }
--- Line.h
+++ Line.h
@@ -9,7 +9,7 @@ class Line {
         Line(Point& p1, Point& p2);
         double getLength();
         double getSlope();
-        bool containsPoint(Point p);
+        bool containsPoint(const Point& p);
         Point getMidpoint();
         void extendLine(double value);
         string toString();
